e62dd462969e44538a2e3b1bb7d5468672ac339c
Richard Green <richardgreen@pursuit.org>
2019-04-27 15:41:32 -0400

    created users route

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const {findit} = require('../services')
+const db = new findit()
+
+
+const usersRouters = {}
+
+usersRouters.create = (req, res)=>{
+    const body = req.body;
+    db.create('users',body)
+    .then(res.json({'msg': 'Successful'}))
+    .catch(err=>{
+        console.log(err)
+        res.json({'error': err})
+    })
+}
+
+
+usersRouters.read = (req, res)=>{
+    const body = req.body
+    db.read('users', body)
+    .then(res.json({'msg': 'Successful'}))
+    .catch((error)=>{
+        console.log(error)
+        res.json({'error': err})
+    })
+}
+
+module.exports = {usersRouters}
